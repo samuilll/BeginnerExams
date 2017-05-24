f1a0313d40b8cd9ff99d87ba625d87ef6919a1b4
samuilll <samuil_dechev@abv.bg>
2017-05-24 14:44:30 +0300

    Not Complete

---
+++ OldExamPreparation3/DNA03/DNA03.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+    class DNA03
+    {
+        static void Main(string[] args)
+        {
+        var N = int.Parse(Console.ReadLine());
+        var FirstLetter = char.Parse(Console.ReadLine());
+        char[] Letters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+        int FirstLetterPLaceInLetters=0;
+        int PointNumber = 3;
+        for (int i = 0; i < Letters.Length; i++)
+        {
+            if (Letters[i] == FirstLetter) FirstLetterPLaceInLetters = i;
+
+        }
+        for (int i = 0; i < N; i++)
+        {
+
+
+            Console.Write("{0}",new string('.',PointNumber));
+            
+        }
+        }
+    }
+
