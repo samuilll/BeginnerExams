16b421888af24054d11f8e677f46dd332b3f85ab
samuilll <samuil_dechev@abv.bg>
2017-06-04 10:51:20 +0300

    Programming Basics Exam - 20 November 2016 - Evening

---
+++ ExamPreparation/ExamPreparation3/FlowerShop02/FlowerShop02.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop02
+{
+    class FlowerShop02
+    {
+        static void Main(string[] args)
+        {
+            var magnoliaCount = int.Parse(Console.ReadLine());
+            var hyacinthCount = int.Parse(Console.ReadLine());
+            var roseCount = int.Parse(Console.ReadLine());
+            var cactusCount = int.Parse(Console.ReadLine());
+            var presentPrice = double.Parse(Console.ReadLine());
+            double[] prices = new double[] { 3.25, 4.00, 3.50, 8 };
+            double profit = magnoliaCount * prices[0] + hyacinthCount * prices[1] + roseCount * prices[2] + cactusCount * prices[3];
+            profit = profit - profit / 20.0;
+            if (profit>=presentPrice)
+                Console.WriteLine("She is left with {0} leva.",Math.Floor(profit-presentPrice));
+            else
+                Console.WriteLine("She will have to borrow {0} leva.",Math.Ceiling(presentPrice-profit));
+                    
+            
+
+        }
+    }
+}
---
+++ ExamPreparation/ExamPreparation3/MaxCombination06/MaxCombination06.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxCombination06
+{
+    class MaxCombination06
+    {
+        static void Main(string[] args)
+        {
+            var start = int.Parse(Console.ReadLine());
+            var end = int.Parse(Console.ReadLine());
+            var stop = int.Parse(Console.ReadLine());
+            var combNumber = 0;
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    if (combNumber == stop) break;
+                    Console.Write("<{0}-{1}>",i,j);
+                    combNumber++;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
