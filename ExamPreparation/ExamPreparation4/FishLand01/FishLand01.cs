d20df6e45bdbe2607214b38de76e9454062780b7
samuilll <samuil_dechev@abv.bg>
2017-06-17 12:20:37 +0300

    Programming Basics Exam - 20 November 2016 - Morning

---
+++ ExamPreparation/ExamPreparation4/FishLand01/FishLand01.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishLand01
+{
+    class FishLand01
+    {
+        static void Main(string[] args)
+        {
+            var skumPrice = double.Parse(Console.ReadLine());
+            var cacaPrice = double.Parse(Console.ReadLine());
+            var palamudKilos = double.Parse(Console.ReadLine());
+            var safridKilos = double.Parse(Console.ReadLine());
+            var midiKilos = double.Parse(Console.ReadLine());
+
+            var palamudPrice =palamudKilos* (skumPrice + 60 * skumPrice / 100);
+            var safridPrice = safridKilos*(cacaPrice + 80 * cacaPrice / 100);
+            var midiPrice = midiKilos * 7.50;
+            var necessaryMoney = palamudPrice + safridPrice + midiPrice;
+
+            Console.WriteLine("{0:f2}",necessaryMoney);
+        }
+    }
+}
