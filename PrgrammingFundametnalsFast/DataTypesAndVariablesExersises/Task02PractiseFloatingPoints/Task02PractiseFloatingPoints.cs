b88d05724afe9fbdf5b3d8a255f17ec551b93b21
samuilll <samuil_dechev@abv.bg>
2017-10-01 09:43:52 +0300

    Commit

--- PrgrammingFundametnalsFast/DataTypesAndVariablesExersises/Task02PractiseFloatingPoints/Task02PractiseFloatingPoints.cs
+++ PrgrammingFundametnalsFast/DataTypesAndVariablesExersises/Task02PractiseFloatingPoints/Task02PractiseFloatingPoints.cs
@@ -5,25 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-    class Task02PractiseFloatingPoints
+class Task02PractiseFloatingPoints
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            sbyte a = -100;
-            byte b = 128;
-            short c = -3540;
-            ushort d = 64876;
-            uint e = 2147483648;
-            int f = -1141583228;
-            long g = -1223372036854775808;
+        decimal a = -3.141592653589793238M;
+        double b = 1.60217657;
+        decimal c =7.8184261974584555216535342341M;
 
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-            Console.WriteLine(e);
-            Console.WriteLine(f);
-            Console.WriteLine(g);
-        }
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+
     }
+}
 
