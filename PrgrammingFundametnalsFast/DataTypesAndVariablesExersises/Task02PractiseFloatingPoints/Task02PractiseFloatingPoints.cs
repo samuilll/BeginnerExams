28faa51d116e2048148427763e31be2078c16b5a
samuilll <samuil_dechev@abv.bg>
2017-10-01 09:30:31 +0300

    Commit

--- PrgrammingFundametnalsFast/DataTypesAndVariablesExersises/Task02PractiseFloatingPoints/Task02PractiseFloatingPoints.cs
+++ PrgrammingFundametnalsFast/DataTypesAndVariablesExersises/Task02PractiseFloatingPoints/Task02PractiseFloatingPoints.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task02PractiseFloatingPoints
-{
-    class Program
+
+    class Task02PractiseFloatingPoints
     {
         static void Main(string[] args)
         {
+            sbyte a = -100;
+            byte b = 128;
+            short c = -3540;
+            ushort d = 64876;
+            uint e = 2147483648;
+            int f = -1141583228;
+            long g = -1223372036854775808;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
         }
     }
-}
+
