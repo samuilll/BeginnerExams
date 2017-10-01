f85c45d1370bcd4d850a684b398b0c3e832e41b6
samuilll <samuil_dechev@abv.bg>
2017-10-01 09:51:14 +0300

    Commit

--- PrgrammingFundametnalsFast/DataTypesAndVariablesExersises/Task02PractiseFloatingPoints/Task02PractiseFloatingPoints.cs
+++ PrgrammingFundametnalsFast/DataTypesAndVariablesExersises/Task02PractiseFloatingPoints/Task02PractiseFloatingPoints.cs
@@ -9,7 +9,7 @@ class Task02PractiseFloatingPoints
 {
     static void Main(string[] args)
     {
-        decimal a = -3.141592653589793238M;
+        decimal a = 3.141592653589793238M;
         double b = 1.60217657;
         decimal c =7.8184261974584555216535342341M;
 
--- PrgrammingFundametnalsFast/DataTypesAndVariablesExersises/Task03PractiseCharsAndStrings/Program.cs
+++ PrgrammingFundametnalsFast/DataTypesAndVariablesExersises/Task03PractiseCharsAndStrings/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task03PractiseCharsAndStrings
-{
+
     class Program
     {
         static void Main(string[] args)
         {
+        string name1 = "Software University";
+        string name2 = "I love programming";
+        char char1 = 'B';
+        char char2 = 'y';
+        char char3 = 'e';
+
+        Console.WriteLine($"{name1}\n{char1}\n{char2}\n{char3}\n{name2}");
         }
     }
-}
+
+
+
+//Software University
+//B
+//y
+//e
+//I love programming
