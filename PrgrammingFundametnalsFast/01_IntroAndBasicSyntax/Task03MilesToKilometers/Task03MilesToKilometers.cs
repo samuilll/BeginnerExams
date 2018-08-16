747f4ee7fc718e471d0937a9549239849e15209b
samuilll <samuil_dechev@abv.bg>
2018-08-16 15:49:46 +0300

    FileNamesChange

---
+++ PrgrammingFundametnalsFast/01_IntroAndBasicSyntax/Task03MilesToKilometers/Task03MilesToKilometers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program //asdsakdjlksajdksajd
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:f2}", a * 1.60934);
+        }
+    }
+}
+//1 mile == 1.60934 kilometers
---
+++ PrgrammingFundametnalsFast/02_ConditionalStatementsAndLoops/Task01ChooseADrink/Task01ChooseADrink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var profession = Console.ReadLine();
+
+        if (profession == "Athlete")
+            Console.WriteLine("Water");
+        else if (profession == "Businessman" || profession == "Businesswoman")
+            Console.WriteLine("Coffee");
+        else if (profession == "SoftUni Student")
+            Console.WriteLine("Beer");
+        else
+            Console.WriteLine("Tea");
+    }
+}
+
+//“Water” – for “Athlete”
+//“Coffee” – for “Businessman” or “Businesswoman”
+//“Beer” – for “SoftUni Student”
+//“Tea” – for all other professions.
